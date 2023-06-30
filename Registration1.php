d4634ec9fab24cde5fea36a865bd41ae10b47918
FonsahPageo <ashprincepageo@gmail.com>
2023-06-30 17:02:47 +0100

    Connected to database

---
+++ Registration1.php
@@ -0,0 +1,65 @@
+<?php
+    require('DB_Connection.php');
+    $fname=  htmlentities($_POST["fname"]);
+    $lname= htmlentities($_POST["lname"]);
+    $email= htmlentities($_POST["email"]);
+    $username= htmlentities($_POST["username"]);
+    $code= htmlentities($_POST["country-code"]);
+    $phone= htmlentities($_POST["phone"]);
+    $gender=htmlentities($_POST["gender"]);
+    $password= htmlentities($_POST["password"]);
+
+    $insert= "INSERT into login (FirstName,LastName,email,Username,CountryCode,PhoneNumber,Gender,Password)
+     VALUES ('$fname','$lname','$email','$username','$code','$phone','$gender','" . md5($password) . "')";
+    $result   = mysqli_query($con, $insert);
+    
+
+    if ($result==1) {
+        echo "<div class='form'>
+           <center><h3>You are registered successfully.</h3><br/>
+            <p class='link'>Click here to <a href='login.html'>Login</a></p>
+            </center></div>";
+    } else {
+            echo "<div class='form'>
+                <center><h3>Registration Unsuccessful.</h3><br/>
+                <p class='link'>Click here to <a href='registration.php'>register</a> again.</p>
+                </center></div>";
+            }
+
+    // When form submitted, insert values into the database.
+    // if (isset($_REQUEST['Username'])) {
+    //     $fname    = stripslashes($_REQUEST['fname']);
+    //     $fname    = mysqli_real_escape_string($con, $fname);
+    //     $lname    = stripslashes($_REQUEST['lname']);
+    //     $lname    = mysqli_real_escape_string($con, $lname);
+    //     $email    = stripslashes($_REQUEST['email']);
+    //     $email    = mysqli_real_escape_string($con, $email);
+    //     // removes backslashes
+    //     $username = stripslashes($_REQUEST['username']);
+    //     //escapes special characters in a string
+    //     $username = mysqli_real_escape_string($con, $username);
+    //     $code    = stripslashes($_REQUEST['country-code']);
+    //     $code    = mysqli_real_escape_string($con, $code);
+    //     $phone    = stripslashes($_REQUEST['phone']);
+    //     $phone  = mysqli_real_escape_string($con, $phone);
+    //     $gender    = stripslashes($_REQUEST['gender']);
+    //     $gender   = mysqli_real_escape_string($con, $gender);
+    //     $password = stripslashes($_REQUEST['password']);
+    //     $password = mysqli_real_escape_string($con, $password);
+    //     // $create_datetime = date("Y-m-d H:i:s");'$create_datetime'
+    //     $query    = "INSERT into `login` (FirstName,LastName,email,Username,CountryCode,PhoneNumber,Gender, Password)
+    //                  VALUES ('$fname','$lname','$email','$username','$code','$phone','$gender', '" . md5($password) . "' )";
+    //     $result   = mysqli_query($con, $query);
+    //     if ($result) {
+    //         echo "<div class='form'>
+    //                 <h3>You are registered successfully.</h3><br/>
+    //                 <p class='link'>Click here to <a href='login.php'>Login</a></p>
+    //               </div>";
+    //     } else {
+    //         echo "<div class='form'>
+    //               <h3>Required fields are missing.</h3><br/>
+    //               <p class='link'>Click here to <a href='registration.php'>registration</a> again.</p>
+    //               </div>";
+    //     }
+    // } else {}
+?>
