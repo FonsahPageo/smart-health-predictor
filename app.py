2c1bdd83ea7c1f928c56dee4792202212e2fee9d
FonsahPageo <ashprincepageo@gmail.com>
2023-07-05 01:45:49 +0100

    Modified app.py, templates folder, added dataset

--- app.py
+++ app.py
@@ -1,70 +1,154 @@
-from flask import Flask, render_template, request
-import flask
+# Import the necessary libraries
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd 
+import os 
+from scipy.stats import mode
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+import joblib
 import pickle
-import json
-from flask_cors import CORS
+import warnings
+from sklearn.exceptions import DataConversionWarning
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split ,cross_val_score
+from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+train_data = pd.read_csv("models/Training.csv").dropna(axis = 1)
+test_data =pd.read_csv("models/Testing.csv").dropna(axis = 1)
+
+encoder = LabelEncoder()
+train_data["prognosis"] =encoder.fit_transform(train_data["prognosis"])
+
+X = train_data.iloc[:,:-1]
+y = train_data.iloc[:,-1]
+X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state =24)
+
+def cv_scoring(estimator,X,y):
+    return accuracy_score(y,estimator.predict(X))
+
+models = {
+    "SVC": SVC(),
+    "Gaussian NB": GaussianNB(),
+    "Random Forest": RandomForestClassifier(random_state=18)
+}
+# producing cross validation score for models
+
+for model_name in models:
+    model =models[model_name]
+    scores = cross_val_score(model,X,y,cv=10,n_jobs = -1,scoring = cv_scoring)
+
+svm_model = SVC()
+svm_model.fit(X_train,y_train)
+preds = svm_model.predict(X_test)
+
+nb_model = GaussianNB()
+nb_model.fit(X_train, y_train)
+preds = nb_model.predict(X_test)
+    
+rf_model = RandomForestClassifier(random_state=18)
+rf_model.fit(X_train, y_train)
+preds = rf_model.predict(X_test)
+
+from statistics import mode
+# Training the models on whole data
+final_svm_model = SVC()
+final_nb_model = GaussianNB()
+final_rf_model = RandomForestClassifier(random_state=18)
+final_svm_model.fit(X, y)
+final_nb_model.fit(X, y)
+final_rf_model.fit(X, y)
+
+test_X = test_data.iloc[:, :-1]
+test_Y = encoder.transform(test_data.iloc[:, -1])
+
+svm_preds = final_svm_model.predict(test_X)
+nb_preds = final_nb_model.predict(test_X)
+rf_preds = final_rf_model.predict(test_X)
+ 
+final_preds = [mode([i, j, k]) for i, j, k in zip(svm_preds, nb_preds, rf_preds)]
+
+# Disable the warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=DataConversionWarning)
+warnings.filterwarnings("ignore", category=UserWarning)
+
+# Initializing Models
+models = {
+    "SVC": SVC(),
+    "Gaussian NB": GaussianNB(),
+    "Random Forest": RandomForestClassifier(random_state=18)
+}
+
+symptoms = X.columns.values
+# Creating a symptom index dictionary to encode the
+# input symptoms into numerical form
+symptom_index = {}
+for index, value in enumerate(symptoms):
+    symptom = " ".join([i.capitalize() for i in value.split("_")])
+    symptom_index[symptom] = index
+
+data_dict = {
+    "symptom_index": symptom_index,
+    "predictions_classes": encoder.classes_
+}
+
+# Defining the Function
+# Input: string containing symptoms separated by commas
+# Output: Generated predictions by models
+def predictDisease(symptoms):
+    symptoms = symptoms.split(",")
+
+    # creating input data for the models
+    input_data = [0] * len(data_dict["symptom_index"])
+    for symptom in symptoms:
+        if symptom in data_dict["symptom_index"]:
+            index = data_dict["symptom_index"][symptom]
+            input_data[index] = 1
+            
+    # reshaping the input data and converting it
+    # into suitable format for model predictions
+    input_data = np.array(input_data).reshape(1, -1)
+    
+    # generating individual outputs
+    rf_prediction = data_dict["predictions_classes"][final_rf_model.predict(input_data)[0]]
+    nb_prediction = data_dict["predictions_classes"][final_nb_model.predict(input_data)[0]]
+    svm_prediction = data_dict["predictions_classes"][final_svm_model.predict(input_data)[0]]
+    
+    # making final prediction by taking mode of all predictions
+    final_prediction = mode([rf_prediction, nb_prediction, svm_prediction])
+    predictions = {
+        "rf_model_prediction": rf_prediction,
+        "naive_bayes_prediction": nb_prediction,
+        "svm_model_prediction": svm_prediction,
+        "final_prediction": final_prediction
+    }
+    return predictions
+
+# Testing the function
+
+# print(predictDisease("Itching,Skin Rash,Nodal Skin Eruptions"))
 
 app = Flask(__name__)
-CORS(app)
 
-with open('models/model.pkl', 'rb') as f:
-    model = pickle.load(f)
+@app.route('/')
+def home():
+    return render_template('predictor.html')
 
-
-@app.route("/")
-def hello():
-    return "Hello, World!"
-
-
-@app.route('/users', methods=["GET", "POST"])
-def users():
-    print("users endpoint reached...")
-    if request.method == "GET":
-        with open("users.json", "r") as f:
-            data = json.load(f)
-            data.append({
-                "username": "user4",
-                "pets": ["hamster"]
-            })
-            return flask.jsonify(data)
-        
-    # if request.method == "POST":
-    #     received_data = request.get_json()
-    #     print(f"received data: {received_data}")
-    #     message = received_data['data']
-    #     return_data = {
-    #         "status": "success",
-    #         "message": f"received: {message}"
-    #     }
-    if request.method == "POST":
-        received_data = request.get_json()
-        print(f"received data: {received_data}")
-        message = received_data['data']
-        # prediction = model.predict([message])
-        prediction = model.predict(["Cough,Fatigue,Vomiting"]   )
-        return_data = {
-            "status": "success",
-            "message": f"received: {message}",
-            "response": prediction[0]
-        }
-        return flask.Response(response=json.dumps(return_data), status=201)
-
-@app.route('/predict', methods=['GET,POST'])
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    print("Prediction endpoint reached...")
-    if request.method == "POST":
-        received_data = request.get_json()
-        print(f"received data: {received_data}")
-        message = received_data['data']
-        return_data = {
-            "status": "success",
-            "message": f"received: {message}",
-            "response": "100%"
-        }
-        return flask.Response(response=json.dumps(return_data), status=201)
-    # input_data = [data['symptom1'], data['symptom2'], data['symptom3']]
-    # prediction = model.predict([input_data])
-    # return render_template('results.html', prediction=prediction[0])
+    if request.method == 'POST':
+        # Get the input data from the form
+        symptoms = request.form['symptoms']
+        # Make a prediction using the machine learning model
+        prediction = predictDisease(symptoms)
+        # Render a template with the prediction results
+        return render_template('result.html', prediction=prediction)
+    else:
+        return render_template('predictor.html')
 
-if __name__ == "__main__":
-    app.run("localhost", 6969)
+if __name__ == '__main__':
+    app.run(debug=True)
