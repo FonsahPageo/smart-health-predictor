8e0615300572f36c5bd9744875564e5918ea57e5
FonsahPageo <ashprincepageo@gmail.com>
2023-07-05 11:08:43 +0100

    Updated medical database

--- database/medical.sql
+++ database/medical.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 08, 2023 at 07:40 PM
--- Server version: 10.3.15-MariaDB
--- PHP Version: 7.3.6
+-- Generation Time: Jul 05, 2023 at 12:07 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -19,37 +18,81 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `health_predictor_db`
+-- Database: `medical`
 --
+CREATE DATABASE IF NOT EXISTS `medical` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `medical`;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `login`
+-- Table structure for table `doctors`
 --
 
-CREATE TABLE `login` (
-  `ID` int(100) NOT NULL,
-  `FirstName` varchar(100) NOT NULL,
-  `LastName` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `Username` varchar(255) NOT NULL,
-  `CountryCode` int(5) NOT NULL,
-  `PhoneNumber` int(50) NOT NULL,
-  `Gender` varchar(10) NOT NULL,
-  `Password` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+DROP TABLE IF EXISTS `doctors`;
+CREATE TABLE `doctors` (
+  `Firstname` int(11) NOT NULL,
+  `Lastname` int(11) NOT NULL,
+  `Email` int(11) NOT NULL,
+  `Password` int(11) NOT NULL,
+  `CountryCode` int(11) NOT NULL,
+  `PhoneNumber` int(11) NOT NULL,
+  `Gender` int(11) NOT NULL,
+  `Location` int(11) NOT NULL,
+  `Specialization` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
 
 --
--- Dumping data for table `login`
+-- Table structure for table `patients`
 --
 
-INSERT INTO `login` (`ID`, `FirstName`, `LastName`, `email`, `Username`, `CountryCode`, `PhoneNumber`, `Gender`, `Password`) VALUES
-(0, '', '', '', 'iMercy', 0, 0, '', '123456789'),
-(0, 'Precious', 'Odi', 'odi@gmail.com', 'Precious', 93, 55555555, 'female', '1234567'),
-(0, 'Queen', 'Esther', 'esther@gmail.com', 'Queen', 355, 2222222, 'female', '25f9e794323b453885f5181f1b624d0b'),
-(0, '', '', '', '', 0, 0, '', ''),
-(0, 'Pageo', 'Leo', 'pageo@gmail.com', 'pageo', 355, 2222222, 'male', 'fcea920f7412b5da7be0cf42b8c93759');
+DROP TABLE IF EXISTS `patients`;
+CREATE TABLE `patients` (
+  `Firstname` text NOT NULL,
+  `Lastname` text NOT NULL,
+  `Email` varchar(11) NOT NULL,
+  `Username` text NOT NULL,
+  `CountryCode` varchar(11) NOT NULL,
+  `PhoneNumber` int(11) NOT NULL,
+  `Gender` text NOT NULL,
+  `Password` varchar(11) NOT NULL,
+  `Id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `patients`
+--
+
+INSERT INTO `patients` (`Firstname`, `Lastname`, `Email`, `Username`, `CountryCode`, `PhoneNumber`, `Gender`, `Password`, `Id`) VALUES
+('chu', 'gana', 'chugana@gma', 'chugana', '237', 677959361, 'male', '23456789', 1),
+('John', 'Syndy', 'johnsyndy@g', 'johnsyndy', '355', 985456785, 'male', 'FASDMSA', 2),
+('ndo', 'kama', 'ndokama@mai', 'ndokama23', '1242', 87656783, 'male', 'adjltewfs', 3),
+('fad', 'kon', 'fadkon@gmai', 'fadkon', '238', 2147483647, 'female', '95dfbc2a96e', 4),
+('FONSAH', 'ASHU', 'ashprincepa', 'AshPrince', '237', 675416098, 'male', 'ashprince', 5),
+('Lum', 'Susan', 'lumsusan@gm', 'LumSuzy', '225', 5247941, 'female', 'LumSusan123', 8);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `patients`
+--
+ALTER TABLE `patients`
+  ADD PRIMARY KEY (`Id`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `patients`
+--
+ALTER TABLE `patients`
+  MODIFY `Id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
