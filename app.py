799f098c13e71b1221bc3b5045a39e340c6a3dac
FonsahPageo <ashprincepageo@gmail.com>
2025-03-05 11:30:38 +0100

    changed python app IP and port

--- app.py
+++ app.py
@@ -242,4 +242,4 @@ def logout():
     return render_template('patient-registration.html')  # Redirect to registration page
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
