bfe579988915c576c553b2768d085c054f924293
FonsahPageo <ashprincepageo@gmail.com>
2025-03-06 12:34:02 +0100

    create patients table if not exists in app.py

--- app.py
+++ app.py
@@ -2,8 +2,8 @@
 from statistics import mode
 from flask import Flask, render_template, request, redirect, url_for, session
 import numpy as np
-import pandas as pd 
-import os 
+import pandas as pd
+import os
 from scipy.stats import mode
 from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
@@ -13,7 +13,7 @@ import pickle
 import warnings
 from sklearn.exceptions import DataConversionWarning
 from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split ,cross_val_score
+from sklearn.model_selection import train_test_split, cross_val_score
 from sklearn.metrics import classification_report, accuracy_score, confusion_matrix
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -22,38 +22,42 @@ from flask_sqlalchemy import SQLAlchemy
 import mysql.connector
 
 # load the training and testing dataset
-train_data = pd.read_csv("models/Training.csv").dropna(axis = 1)
-test_data =pd.read_csv("models/Testing.csv").dropna(axis = 1)
+train_data = pd.read_csv("models/Training.csv").dropna(axis=1)
+test_data = pd.read_csv("models/Testing.csv").dropna(axis=1)
 
 encoder = LabelEncoder()
-train_data["prognosis"] =encoder.fit_transform(train_data["prognosis"])
+train_data["prognosis"] = encoder.fit_transform(train_data["prognosis"])
 
-X = train_data.iloc[:,:-1]
-y = train_data.iloc[:,-1]
-X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2,random_state =24)
+X = train_data.iloc[:, :-1]
+y = train_data.iloc[:, -1]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=24
+)
 
-def cv_scoring(estimator,X,y):
-    return accuracy_score(y,estimator.predict(X))
+
+def cv_scoring(estimator, X, y):
+    return accuracy_score(y, estimator.predict(X))
+
 
 models = {
     "SVC": SVC(),
     "Gaussian NB": GaussianNB(),
-    "Random Forest": RandomForestClassifier(random_state=18)
+    "Random Forest": RandomForestClassifier(random_state=18),
 }
 
 # producing cross validation score for models
 for model_name in models:
-    model =models[model_name]
-    scores = cross_val_score(model,X,y,cv=10,n_jobs = -1,scoring = cv_scoring)
+    model = models[model_name]
+    scores = cross_val_score(model, X, y, cv=10, n_jobs=-1, scoring=cv_scoring)
 
 svm_model = SVC()
-svm_model.fit(X_train,y_train)
+svm_model.fit(X_train, y_train)
 preds = svm_model.predict(X_test)
 
 nb_model = GaussianNB()
 nb_model.fit(X_train, y_train)
 preds = nb_model.predict(X_test)
-    
+
 rf_model = RandomForestClassifier(random_state=18)
 rf_model.fit(X_train, y_train)
 preds = rf_model.predict(X_test)
@@ -74,7 +78,7 @@ test_Y = encoder.transform(test_data.iloc[:, -1])
 svm_preds = final_svm_model.predict(test_X)
 nb_preds = final_nb_model.predict(test_X)
 rf_preds = final_rf_model.predict(test_X)
- 
+
 final_preds = [mode([i, j, k]) for i, j, k in zip(svm_preds, nb_preds, rf_preds)]
 
 # Disable the warnings
@@ -86,7 +90,7 @@ warnings.filterwarnings("ignore", category=UserWarning)
 models = {
     "SVC": SVC(),
     "Gaussian NB": GaussianNB(),
-    "Random Forest": RandomForestClassifier(random_state=18)
+    "Random Forest": RandomForestClassifier(random_state=18),
 }
 
 symptoms = X.columns.values
@@ -98,10 +102,8 @@ for index, value in enumerate(symptoms):
     symptom = " ".join([i.capitalize() for i in value.split("_")])
     symptom_index[symptom] = index
 
-data_dict = {
-    "symptom_index": symptom_index,
-    "predictions_classes": encoder.classes_
-}
+data_dict = {"symptom_index": symptom_index, "predictions_classes": encoder.classes_}
+
 
 # Defining the Function
 # Input: string containing symptoms separated by commas
@@ -115,131 +117,171 @@ def predictDisease(symptoms):
         if symptom in data_dict["symptom_index"]:
             index = data_dict["symptom_index"][symptom]
             input_data[index] = 1
-            
+
     # reshaping the input data and converting it
     # into suitable format for model predictions
     input_data = np.array(input_data).reshape(1, -1)
-    
+
     # generating individual outputs
-    rf_prediction = data_dict["predictions_classes"][final_rf_model.predict(input_data)[0]]
-    nb_prediction = data_dict["predictions_classes"][final_nb_model.predict(input_data)[0]]
-    svm_prediction = data_dict["predictions_classes"][final_svm_model.predict(input_data)[0]]
-    
+    rf_prediction = data_dict["predictions_classes"][
+        final_rf_model.predict(input_data)[0]
+    ]
+    nb_prediction = data_dict["predictions_classes"][
+        final_nb_model.predict(input_data)[0]
+    ]
+    svm_prediction = data_dict["predictions_classes"][
+        final_svm_model.predict(input_data)[0]
+    ]
+
     # making final prediction by taking mode of all predictions
     final_prediction = mode([rf_prediction, nb_prediction, svm_prediction])
     predictions = {
         "RF model prediction": rf_prediction,
         "Naive Bayes prediction": nb_prediction,
         "SVM model prediction": svm_prediction,
-        "Final prediction": final_prediction
+        "Final prediction": final_prediction,
     }
     return predictions
+
 
 # start flask application
 app = Flask(__name__)
 
-app.secret_key = 'ash_key'
+app.secret_key = "ash_key"
 db = mysql.connector.connect(
-    host='mysql',
-    user='user',
-    password='userpassword',
-    database='medical'
+    host="mysql", user="user", password="userpassword", database="medical"
 )
 
-# defining the different routes of the website    
-@app.route('/', methods=['GET', 'POST'])
+
+# defining the different routes of the website
+@app.route("/", methods=["GET", "POST"])
 def registration():
-    if request.method == 'POST':
-        if 'login' in request.form:
-            return redirect(url_for('login'))
-        
-        firstname = request.form['fname']
-        lastname = request.form['lname']
-        email = request.form['email']
-        username = request.form['username']
-        countrycode = request.form['country-code']
-        phonenumber = request.form['phone']
-        gender = request.form['gender']
-        password = request.form['password']
-        
+    create_table_query = """
+    CREATE TABLE IF NOT EXISTS patients (
+        ID INT AUTO_INCREMENT PRIMARY KEY,
+        Firstname VARCHAR(255) NOT NULL,
+        Lastname VARCHAR(255) NOT NULL,
+        Email VARCHAR(255) UNIQUE NOT NULL,
+        Username VARCHAR(255) UNIQUE NOT NULL,
+        CountryCode VARCHAR(10),
+        PhoneNumber VARCHAR(20),
+        Gender VARCHAR(10),
+        Password VARCHAR(255) NOT NULL
+    )
+    """
+    cursor.execute(create_table_query)
+    db.commit()
+
+    if request.method == "POST":
+        if "login" in request.form:
+            return redirect(url_for("login"))
+
+        firstname = request.form["fname"]
+        lastname = request.form["lname"]
+        email = request.form["email"]
+        username = request.form["username"]
+        countrycode = request.form["country-code"]
+        phonenumber = request.form["phone"]
+        gender = request.form["gender"]
+        password = request.form["password"]
+
         cursor = db.cursor()
         query = "INSERT INTO patients (Firstname, Lastname, Email, Username, CountryCode, PhoneNumber, Gender, Password) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
-        values = (firstname, lastname, email, username, countrycode, phonenumber, gender, password)
+        values = (
+            firstname,
+            lastname,
+            email,
+            username,
+            countrycode,
+            phonenumber,
+            gender,
+            password,
+        )
         cursor.execute(query, values)
         db.commit()
-        return redirect(url_for('login'))
-    
-    return render_template('patient-registration.html')
+        return redirect(url_for("login"))
 
-@app.route('/login', methods=['GET', 'POST'])
+    return render_template("patient-registration.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
         cursor = db.cursor()
         query = "SELECT * FROM patients WHERE username = %s AND password = %s"
         values = (username, password)
         cursor.execute(query, values)
         user = cursor.fetchone()
         if user:
-            session['username'] = username
-            return redirect(url_for('index'))
+            session["username"] = username
+            return redirect(url_for("index"))
         else:
-            return render_template('login.html', error='Invalid username or password')
-    return render_template('login.html')
+            return render_template("login.html", error="Invalid username or password")
+    return render_template("login.html")
 
-@app.route('/index')
+
+@app.route("/index")
 def index():
     # require user to be logged in before accessing the platform
-    if 'username' in session:
-        return render_template('index.html')
+    if "username" in session:
+        return render_template("index.html")
     else:
-        return redirect(url_for('login'))
+        return redirect(url_for("login"))
 
-@app.route('/consultation')
+
+@app.route("/consultation")
 def consultation():
-    return render_template('consultation.html')
+    return render_template("consultation.html")
 
-@app.route('/news')
+
+@app.route("/news")
 def news():
-    return render_template('news.html')
+    return render_template("news.html")
 
-@app.route('/patient')
+
+@app.route("/patient")
 def patient():
-    return render_template('patient.html')
+    return render_template("patient.html")
 
-@app.route('/predictor')
+
+@app.route("/predictor")
 def predictor():
-    return render_template('predictor.html')
+    return render_template("predictor.html")
 
-@app.route('/predict', methods=['GET', 'POST'])
+
+@app.route("/predict", methods=["GET", "POST"])
 def predict():
-    if request.method == 'POST':
+    if request.method == "POST":
         # Get the input data from the form
-        symptom_list = request.form.getlist('symptoms')
-        symptoms = ', '.join(symptom_list)
+        symptom_list = request.form.getlist("symptoms")
+        symptoms = ", ".join(symptom_list)
         # Make a prediction using the machine learning model
         prediction = predictDisease(symptoms)
         # Render a template with the prediction results
-        return render_template('result.html', prediction=prediction)
+        return render_template("result.html", prediction=prediction)
     else:
-        return render_template('predictor.html')
+        return render_template("predictor.html")
 
-@app.route('/profile/<string:username>')
+
+@app.route("/profile/<string:username>")
 def profile(user_id):
     cursor = db.cursor()
     query = "SELECT * FROM users WHERE username = %s"
     values = (user_id,)
     cursor.execute(query, values)
     user = cursor.fetchone()
-    return render_template('patient.html', user=user)
+    return render_template("patient.html", user=user)
 
-@app.route('/logout')
+
+@app.route("/logout")
 def logout():
     # Perform logout actions
     session.clear()  # Clear session data
     # Additional logout tasks
-    return render_template('patient-registration.html')  # Redirect to registration page
+    return render_template("patient-registration.html")  # Redirect to registration page
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
